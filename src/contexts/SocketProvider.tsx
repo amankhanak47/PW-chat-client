543b79cd5f2b54e7932fb7e613165ee31d5300d6
SAI CHARAN BHOGI <saicharanbhogi2001@gmail.com>
2024-01-05 13:36:41 +0530

    Fix

--- src/contexts/SocketProvider.tsx
+++ src/contexts/SocketProvider.tsx
@@ -2,124 +2,126 @@ import React, { useCallback, useContext, useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
 interface SocketProviderProps {
-	children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 interface ISocketContext {
-	sendMessage: (msg: string) => any;
-	getMessages: () => any;
-	messages: string[];
-	currentUserID: string;
-	setCurrentUserID: any;
-	initialize: any;
+  socket: any;
+  sendMessage: (msg: string) => any;
+  getMessages: () => any;
+  messages: string[];
+  currentUserID: string;
+  setCurrentUserID: any;
+  initialize: any;
 }
 
 interface Button {
-	code: string;
-	label: string;
-	link_url: string;
-	action_taken: boolean;
+  code: string;
+  label: string;
+  link_url: string;
+  action_taken: boolean;
 }
 
 export interface Message {
-	sender: number;
-	receiver: number;
-	content: string;
-	sent_at: string;
-	type: string;
-	card_data?: any;
-	buttons?: Button[];
+  sender: number;
+  receiver: number;
+  content: string;
+  sent_at: string;
+  type: string;
+  card_data?: any;
+  buttons?: Button[];
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
 export const useSocket = () => {
-	const state = useContext(SocketContext);
-	if (!state) throw new Error(`state is undefined`);
+  const state = useContext(SocketContext);
+  if (!state) throw new Error(`state is undefined`);
 
-	return state;
+  return state;
 };
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
-	const [socket, setSocket] = useState<Socket>();
-	const [messages, setMessages] = useState<Message[]>([]);
-	const [currentUserID, setCurrentUserID] = useState<string>("1");
+  const [socket, setSocket] = useState<Socket>();
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [currentUserID, setCurrentUserID] = useState<string>("1");
 
-	const onFetchMessages = useCallback((messages: Message[]) => {
-		setMessages(messages);
-	}, []);
+  const onFetchMessages = useCallback((messages: Message[]) => {
+    setMessages(messages);
+  }, []);
 
-	const getMessages: ISocketContext["getMessages"] = useCallback(() => {
-		if (socket) {
-			socket.emit("fetch messages", { userID: 1 }, onFetchMessages);
-		}
-	}, [socket, onFetchMessages]);
+  const getMessages: ISocketContext["getMessages"] = useCallback(() => {
+    if (socket) {
+      socket.emit("fetch messages", { userID: 1 }, onFetchMessages);
+    }
+  }, [socket, onFetchMessages]);
 
-	const onInitialize = useCallback(() => {
-		getMessages();
-	}, [getMessages]);
+  const onInitialize = useCallback(() => {
+    getMessages();
+  }, [getMessages]);
 
-	const initialize: ISocketContext["initialize"] = useCallback(
-		(timezone: string) => {
-			console.log("Initialize", currentUserID, timezone);
-			if (socket && currentUserID) {
-				socket.emit(
-					"initialize",
-					{ userID: currentUserID, timezone },
-					onInitialize
-				);
-			}
-		},
-		[currentUserID, onInitialize, socket]
-	);
+  const initialize: ISocketContext["initialize"] = useCallback(
+    (timezone: string) => {
+      console.log("Initialize", currentUserID, timezone);
+      if (socket && currentUserID) {
+        socket.emit(
+          "initialize",
+          { userID: currentUserID, timezone },
+          onInitialize
+        );
+      }
+    },
+    [currentUserID, onInitialize, socket]
+  );
 
-	const sendMessage: ISocketContext["sendMessage"] = useCallback(
-		(message) => {
-			console.log("Send Message", {
-				message,
-				to: currentUserID == "1" ? "2" : "1",
-			});
-			if (socket) {
-				socket.emit("message", {
-					message,
-					to: currentUserID == "1" ? "2" : "1",
-					userID: currentUserID,
-				});
-			}
-		},
-		[currentUserID, socket]
-	);
+  const sendMessage: ISocketContext["sendMessage"] = useCallback(
+    (message) => {
+      console.log("Send Message", {
+        message,
+        to: currentUserID == "1" ? "2" : "1",
+      });
+      if (socket) {
+        socket.emit("message", {
+          message,
+          to: currentUserID == "1" ? "2" : "1",
+          userID: currentUserID,
+        });
+      }
+    },
+    [currentUserID, socket]
+  );
 
-	const onMessageRec = useCallback((message: Message) => {
-		console.log("From Server Msg Rec", message);
-		setMessages((prev) => [...prev, message]);
-	}, []);
+  const onMessageRec = useCallback((message: Message) => {
+    console.log("From Server Msg Rec", message);
+    setMessages((prev) => [...prev, message]);
+  }, []);
 
-	useEffect(() => {
-		const _socket = io("http://localhost:8000");
-		_socket.on("message", onMessageRec);
+  useEffect(() => {
+    const _socket = io("http://localhost:8000");
+    _socket.on("message", onMessageRec);
 
-		setSocket(_socket);
+    setSocket(_socket);
 
-		return () => {
-			_socket.off("message", onMessageRec);
-			_socket.disconnect();
-			setSocket(undefined);
-		};
-	}, [onMessageRec]);
+    return () => {
+      _socket.off("message", onMessageRec);
+      _socket.disconnect();
+      setSocket(undefined);
+    };
+  }, [onMessageRec]);
 
-	return (
-		<SocketContext.Provider
-			value={{
-				sendMessage,
-				receivedMessages: messages,
-				getMessages,
-				currentUserID,
-				setCurrentUserID,
-				initialize,
-			}}
-		>
-			{children}
-		</SocketContext.Provider>
-	);
+  return (
+    <SocketContext.Provider
+      value={{
+        socket,
+        sendMessage,
+        receivedMessages: messages,
+        getMessages,
+        currentUserID,
+        setCurrentUserID,
+        initialize,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
 };
