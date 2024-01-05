d6e4b8d49ed049c32e8339701cbcc1aa5686f28a
SAI CHARAN BHOGI <saicharanbhogi2001@gmail.com>
2024-01-05 10:00:35 +0530

    Style changes

--- src/Components/Message/index.tsx
+++ src/Components/Message/index.tsx
@@ -6,49 +6,49 @@ import { StyledMessage } from "./style";
 const users = { 1: "CEO", 2: "Team Member" };
 
 function MessageComponent({
-	m,
-	selectedTimeZone,
+  m,
+  selectedTimeZone,
 }: {
-	m: Message;
-	selectedTimeZone: string;
+  m: Message;
+  selectedTimeZone: string;
 }) {
-	// console.log(this, "ldsk")
-	const { currentUserID } = useSocket();
-	const direction = m.sender == currentUserID ? "right" : "left";
-	console.log(m.buttons);
+  // console.log(this, "ldsk")
+  const { currentUserID } = useSocket();
+  const direction = m.sender == currentUserID ? "right" : "left";
+  console.log(m.buttons);
 
-	return (
-		<StyledMessage direction={direction}>
-			<div className={`message-content ${direction}`} id={m.uuid}>
-				<div className="message-body">
-					<p>{m.content}</p>
-					{m.buttons && m.buttons.length > 0 && m.buttons[0].label != null && (
-						<div className="buttons">
-							{m.buttons.map((btn: any) => {
-								return (
-									<Link to={btn.url} className="redirect-login">
-										<button className="chat-btn">{btn.label}</button>
-									</Link>
-								);
-							})}
-						</div>
-					)}
-				</div>
-				<div className="message-footer" >
-					<p>{users[m.sender]}</p>
-					<p>
-						{convertToSelectedTimeZone(m.sent_at, selectedTimeZone)}
-						<span className="double-tick">
-							<img
-								src="https://r.lykkeworks.com/assets/doubletick-66431f34.svg"
-								alt=""
-							/>
-						</span>
-					</p>
-				</div>
-			</div>
-		</StyledMessage>
-	);
+  return (
+    <StyledMessage direction={direction}>
+      <div className={`message-content ${direction}`} id={m.uuid}>
+        <div className="message-body">
+          <p>{m.content}</p>
+          {m.buttons && m.buttons.length > 0 && m.buttons[0].label != null && (
+            <div className="buttons">
+              {m.buttons.map((btn: any) => {
+                return (
+                  <Link to={btn.url} className="redirect-login">
+                    <button className="chat-btn">{btn.label}</button>
+                  </Link>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        <div className="message-footer">
+          <p>{users[m.sender]}</p>
+          <p>
+            {convertToSelectedTimeZone(m.sent_at, selectedTimeZone)}
+            <span className="double-tick">
+              <img
+                src="https://r.lykkeworks.com/assets/doubletick-66431f34.svg"
+                alt=""
+              />
+            </span>
+          </p>
+        </div>
+      </div>
+    </StyledMessage>
+  );
 }
 
 export default MessageComponent;
--- src/Components/Message/style.ts
+++ src/Components/Message/style.ts
@@ -21,18 +21,18 @@ export const StyledMessage = styled.div<MessageProps>`
     border-radius: 0.75rem 0.75rem 0.75rem 0px;
     width: 450px;
   }
-  .message-body{
+  .message-body {
     display: flex;
     flex-direction: column;
     gap: 10px;
+    overflow-wrap: anywhere;
   }
-  .buttons{
+  .buttons {
     display: flex;
     gap: 1rem;
     justify-content: end;
-
   }
-  .chat-btn{
+  .chat-btn {
     font-weight: 500;
     border: none;
     font-size: 16px;
@@ -43,9 +43,10 @@ export const StyledMessage = styled.div<MessageProps>`
     color: rgb(248, 251, 255);
     background-color: rgb(83, 148, 255);
     cursor: pointer;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 0.5px -1px, rgba(0, 0, 0, 0.1) 0px 1px 2px 0px, rgba(0, 0, 0, 0.08) 0px 0px 2px 0px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 0.5px -1px,
+      rgba(0, 0, 0, 0.1) 0px 1px 2px 0px, rgba(0, 0, 0, 0.08) 0px 0px 2px 0px;
   }
-  .message-footer{
+  .message-footer {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -54,8 +55,7 @@ export const StyledMessage = styled.div<MessageProps>`
     padding: 0px;
     margin-top: 12px;
   }
-  .double-tick{
+  .double-tick {
     margin-left: 4px;
   }
 `;
-
