102af285de7a594b3e4399bae952d71cb4d31863
SAI CHARAN BHOGI <saicharanbhogi2001@gmail.com>
2024-01-11 16:05:27 +0530

    Added Markup

--- src/Components/chat/index.tsx
+++ src/Components/chat/index.tsx
@@ -1,179 +1,247 @@
 import {
-	FunctionComponent,
-	SetStateAction,
-	useEffect,
-	useMemo,
-	useState,
+  FunctionComponent,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
 } from "react";
 import ChatMessage from "../ChatMessage";
 import { useSocket } from "../../contexts/SocketProvider";
 import InitialConfigurationDialog from "../InitialConfigurationDialog";
 import TextField from "@mui/material/TextField";
 import {
-	Box,
-	IconButton,
-	List,
-	ListItem,
-	ListItemButton,
-	ListItemText,
-	ListSubheader,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListSubheader,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { ChatPageContainer } from "./style";
 import { Message } from "../../types/message";
+// import {
+//   MDXEditor,
+//   headingsPlugin,
+//   markdownShortcutPlugin,
+//   frontmatterPlugin,
+//   thematicBreakPlugin,
+//   tablePlugin,
+//   imagePlugin,
+//   linkDialogPlugin,
+//   linkPlugin,
+//   quotePlugin,
+//   listsPlugin,
+//   MDXEditorMethods,
+//   BoldItalicUnderlineToggles,
+//   UndoRedo,
+//   toolbarPlugin,
+//   KitchenSinkToolbar,
+// } from "@mdxeditor/editor";
+// import { MoreHoriz } from "@mui/icons-material";
+
+//...
 
 type ChatProps = {
-	initial: boolean;
+  initial: boolean;
 };
 
 type Command = {
-	name: string;
+  name: string;
 };
 
 type CommandDialogProps = {
-	message: string;
-	commands: Command[];
+  message: string;
+  commands: Command[];
 };
 
 const renderMessages = (messages: Message[], selectedTimezone: string) => {
-	return (
-		messages &&
-		messages.map((m: Message) => {
-			return <ChatMessage message={m} selectedTimeZone={selectedTimezone} />;
-		})
-	);
+  return (
+    messages &&
+    messages.map((m: Message) => {
+      return (
+        <ChatMessage
+          key={m.uuid}
+          message={m}
+          selectedTimeZone={selectedTimezone}
+        />
+      );
+    })
+  );
 };
 
 const CommandDialog: FunctionComponent<CommandDialogProps> = ({
-	message,
-	commands,
+  message,
+  commands,
 }) => {
-	const commandsList = useMemo(() => {
-		const searchKey = message.replace("/", "").toLowerCase().trim();
-		if (searchKey == "") return commands;
+  const commandsList = useMemo(() => {
+    const searchKey = message.replace("/", "").toLowerCase().trim();
+    if (searchKey == "") return commands;
 
-		const filteredCommands = commands.filter((c) =>
-			c.name.toLowerCase().includes(searchKey)
-		);
+    const filteredCommands = commands.filter((c) =>
+      c.name.toLowerCase().includes(searchKey)
+    );
 
-		return filteredCommands;
-	}, [commands, message]);
+    return filteredCommands;
+  }, [commands, message]);
 
-	return (
-		<Box sx={{ background: "#fff" }}>
-			<List
-				subheader={
-					<ListSubheader id="nested-list-subheader">
-						Available Actions
-					</ListSubheader>
-				}
-				dense
-			>
-				{commandsList && commandsList.length > 0 ? (
-					commandsList.map((command) => (
-						<ListItem>
-							<ListItemButton>
-								<ListItemText primary={command.name} />
-							</ListItemButton>
-						</ListItem>
-					))
-				) : (
-					<ListItem>No matching commands</ListItem>
-				)}
-			</List>
-		</Box>
-	);
+  return (
+    <Box sx={{ background: "#fff" }}>
+      <List
+        subheader={
+          <ListSubheader id="nested-list-subheader">
+            Available Actions
+          </ListSubheader>
+        }
+        dense
+      >
+        {commandsList && commandsList.length > 0 ? (
+          commandsList.map((command) => (
+            <ListItem>
+              <ListItemButton>
+                <ListItemText primary={command.name} />
+              </ListItemButton>
+            </ListItem>
+          ))
+        ) : (
+          <ListItem>No matching commands</ListItem>
+        )}
+      </List>
+    </Box>
+  );
 };
 
 const Chat = ({ initial }: ChatProps) => {
-	const { sendMessage, receivedMessages } = useSocket();
-	const [open, setOpen] = useState(initial);
-	const [selectedTimeZone, setSelectedTimeZone] = useState("America/New_York");
-	const [message, setMessage] = useState<string>("");
-	const [showCommandDialog, setShowCommandDialog] = useState<boolean>(false);
-	const [commands, setCommands] = useState([
-		{ name: "Update My Goals" },
-		{ name: "View History" },
-		{ name: "View Goals and Comment Updates" },
-	]);
+  const { sendMessage, receivedMessages } = useSocket();
+  const [open, setOpen] = useState(initial);
+  const [selectedTimeZone, setSelectedTimeZone] = useState("America/New_York");
+  const [message, setMessage] = useState<string>("");
+  const [showCommandDialog, setShowCommandDialog] = useState<boolean>(false);
+  const editorRef = useRef<MDXEditorMethods>(null);
+  const inputRef = useRef<HTMLInputElement>();
+  const [commands] = useState([
+    { name: "Update My Goals" },
+    { name: "View History" },
+    { name: "View Goals and Comment Updates" },
+  ]);
 
-	useEffect(() => {
-		if (receivedMessages) {
-			const lastIndex = receivedMessages.length - 1;
-			const lastMessage = receivedMessages[lastIndex];
-			const parentMessage = document.getElementById(lastMessage?.uuid);
-			parentMessage?.scrollIntoView({ behavior: "instant", block: "end" });
-		}
-	}, [receivedMessages]);
+  const [openToolbar, setToolBar] = useState(false);
 
-	const onSend = () => {
-		sendMessage(message.trim());
-		setMessage("");
-	};
+  useEffect(() => {
+    if (receivedMessages) {
+      const lastIndex = receivedMessages.length - 1;
+      const lastMessage = receivedMessages[lastIndex];
+      const parentMessage = document.getElementById(lastMessage?.uuid);
+      parentMessage?.scrollIntoView({ behavior: "instant", block: "end" });
+    }
+  }, [receivedMessages]);
 
-	const setTimeZone = (value: SetStateAction<string>) => {
-		setSelectedTimeZone(value);
-	};
+  const onSend = () => {
+    sendMessage(message.trim());
+    setMessage("");
+    editorRef.current?.setMarkdown("");
+  };
 
-	const onMessageChange = (event: any) => {
-		if (event.target.value && event.target.value.startsWith("/"))
-			setShowCommandDialog(true);
-		else setShowCommandDialog(false);
-		setMessage(event.target.value);
-	};
+  const setTimeZone = (value: SetStateAction<string>) => {
+    setSelectedTimeZone(value);
+  };
 
-	// const generateForm = () => {
-	// 	socket.emit("generate_form", {
-	// 		message: "goal Update",
-	// 		data: { initial: "10", taget: "0" },
-	// 		to: currentUserID == "1" ? "2" : "1",
-	// 		userID: currentUserID,
-	// 	});
-	// };
+  const onMessageChange = (event: any, usedFor?: string) => {
+    if (usedFor) {
+      if (event && event.startsWith("/")) setShowCommandDialog(true);
+      else setShowCommandDialog(false);
+      setMessage(event);
+    } else {
+      if (event && event.target.value.startsWith("/"))
+        setShowCommandDialog(true);
+      else setShowCommandDialog(false);
+      setMessage(event.target.value);
+      editorRef?.current?.setMarkdown(inputRef?.current!.value);
+    }
+  };
 
-	// const generateButtons = () => {
-	// 	socket.emit("generate_buttons", {
-	// 		message: "goal Update",
-	// 		data: { initial: "10", taget: "0" },
-	// 		to: currentUserID == "1" ? "2" : "1",
-	// 		userID: currentUserID,
-	// 	});
-	// };
+  // const generateForm = () => {
+  // 	socket.emit("generate_form", {
+  // 		message: "goal Update",
+  // 		data: { initial: "10", taget: "0" },
+  // 		to: currentUserID == "1" ? "2" : "1",
+  // 		userID: currentUserID,
+  // 	});
+  // };
 
-	return (
-		<ChatPageContainer>
-			<div className="chat-header">
-				{/* <Button onClick={generateForm}>Generate Form</Button>
+  // const generateButtons = () => {
+  // 	socket.emit("generate_buttons", {
+  // 		message: "goal Update",
+  // 		data: { initial: "10", taget: "0" },
+  // 		to: currentUserID == "1" ? "2" : "1",
+  // 		userID: currentUserID,
+  // 	});
+  // };
+
+  return (
+    <ChatPageContainer>
+      <div className="chat-header">
+        {/* <Button onClick={generateForm}>Generate Form</Button>
 				<Button onClick={generateButtons}>Generate Buttons</Button> */}
-			</div>
-			<div className="chat-messages-container">
-				{renderMessages(receivedMessages, selectedTimeZone)}
-			</div>
-			{showCommandDialog && (
-				<CommandDialog commands={commands} message={message} />
-			)}
-			<Box display={"flex"}>
-				<TextField
-					onChange={onMessageChange}
-					onKeyDown={(e) => {
-						if (e.key === "Enter") onSend();
-					}}
-					value={message}
-					size="small"
-					placeholder="Type / to view available actions"
-					fullWidth
-				/>
-				<IconButton onClick={onSend}>
-					<SendIcon />
-				</IconButton>
-			</Box>
-			<InitialConfigurationDialog
-				open={open}
-				close={setOpen}
-				setTimeZone={setTimeZone}
-			/>
-		</ChatPageContainer>
-	);
+      </div>
+      <div className="chat-messages-container">
+        {renderMessages(receivedMessages, selectedTimeZone)}
+      </div>
+      {showCommandDialog && (
+        <CommandDialog commands={commands} message={message} />
+      )}
+      <Box display={"flex"}>
+        {/* <IconButton onClick={() => setToolBar((p) => !p)}>
+          <MoreHoriz />
+        </IconButton> */}
+        {openToolbar ? (
+          //   <MDXEditor
+          //     className="editor dark-theme dark-editor"
+          //     markdown={message}
+          //     autoFocus={true}
+          //     ref={editorRef}
+          //     plugins={[
+          //       listsPlugin(),
+          //       quotePlugin(),
+          //       headingsPlugin(),
+          //       linkPlugin(),
+          //       linkDialogPlugin(),
+          //       imagePlugin(),
+          //       tablePlugin(),
+          //       thematicBreakPlugin(),
+          //       frontmatterPlugin(),
+          //       markdownShortcutPlugin(),
+          //     ]}
+          //     onChange={(e) => onMessageChange(e, "editor")}
+          //   />
+          ""
+        ) : (
+          <TextField
+            inputRef={inputRef}
+            onChange={(e) => onMessageChange(e)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") onSend();
+            }}
+            value={message}
+            size="small"
+            placeholder="Type / to view available actions"
+            fullWidth
+          />
+        )}
+
+        <IconButton onClick={onSend}>
+          <SendIcon />
+        </IconButton>
+      </Box>
+      <InitialConfigurationDialog
+        open={open}
+        close={setOpen}
+        setTimeZone={setTimeZone}
+      />
+    </ChatPageContainer>
+  );
 };
 
 export default Chat;
--- src/Components/chat/style.ts
+++ src/Components/chat/style.ts
@@ -1,47 +1,59 @@
 import styled from "@emotion/styled";
 
 export const ChatPageContainer = styled.div`
-	padding: 24px;
-	margin: 0;
-	background: #f8fbff;
-	height: 100vh;
-	width: 100vw;
-	box-sizing: border-box;
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
-	.chat-messages-container {
-		box-sizing: border-box;
-		flex: 1;
-		display: flex;
-		gap: 1rem;
-		padding: 1rem;
-		overflow-y: scroll;
-		flex-direction: column;
-	}
+  padding: 24px;
+  margin: 0;
+  background: #f8fbff;
+  height: 100vh;
+  width: 100vw;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  .chat-messages-container {
+    box-sizing: border-box;
+    flex: 1;
+    display: flex;
+    gap: 1rem;
+    padding: 1rem;
+    overflow-y: scroll;
+    flex-direction: column;
+  }
 
-	.input-container {
-		width: min(72rem, 100%);
-		box-sizing: border-box;
-		height: fit-content;
-		display: flex;
-		background-color: rgb(255, 255, 255);
-		position: relative;
-		margin: 0px auto;
-		border-radius: 0.25rem;
-		filter: drop-shadow(rgba(0, 0, 0, 0.1) 2px 2px 4px);
-		position: sticky;
-		bottom: 2.187rem;
-	}
+  .input-container {
+    width: min(72rem, 100%);
+    box-sizing: border-box;
+    height: fit-content;
+    display: flex;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    margin: 0px auto;
+    border-radius: 0.25rem;
+    filter: drop-shadow(rgba(0, 0, 0, 0.1) 2px 2px 4px);
+    position: sticky;
+    bottom: 2.187rem;
+  }
 
-	.input {
-		flex: 1 1 0%;
-		display: flex;
-		-webkit-box-align: center;
-		align-items: center;
-		padding: 10px 16px;
-		border-radius: 0.25rem;
-		border: none;
-		outline: none;
-	}
+  .input {
+    flex: 1 1 0%;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    padding: 10px 16px;
+    border-radius: 0.25rem;
+    border: none;
+    outline: none;
+  }
+  .editor {
+    width: 100%;
+    border: 1px solid grey;
+    border-radius: 4px;
+    background-color: #fff;
+  }
+  ._rootContentEditableWrapper_11eqz_1047,
+  ._contentEditable_11eqz_352 {
+    height: 100%;
+    align-items: center;
+    overflow: hidden;
+  }
 `;
